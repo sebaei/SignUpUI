2a73b76346b80335aae52614d5af5b4477f9131d
Youssef Khaled El Sebaei <47517850+sebaei@users.noreply.github.com>
2021-12-09 21:47:05 +0200

    Add files via upload

---
+++ components/Personal.js
@@ -0,0 +1,73 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
+
+import Eye from 'react-native-vector-icons/FontAwesome';
+
+const Personal = () => {
+  const [toggleCheckBox, setToggleCheckBox] = useState(false);
+  const [textinput1, setTextinput1] = useState('');
+  const [textinput2, setTextinput2] = useState('');
+  const [textinput3, setTextinput3] = useState('');
+  const [textinput4, setTextinput4] = useState('');
+  const [textinput5, setTextinput5] = useState('');
+  const [textinput6, setTextinput6] = useState('');
+
+  return (
+    <View>
+      <TextInput
+        onChangeText={text => setTextinput1(text)}
+        placeholder="First Name"
+        style={styles.input}></TextInput>
+      <TextInput
+        onChangeText={text => setTextinput2(text)}
+        placeholder="Last Name"
+        style={styles.input}></TextInput>
+      <TextInput
+        onChangeText={text => setTextinput3(text)}
+        placeholder="E-mail"
+        style={styles.input}></TextInput>
+      <TextInput
+        keyboardType="numeric"
+        onChange={number => setTextinput4(number)}
+        placeholder="Phone Number"
+        style={styles.input}></TextInput>
+      <TextInput
+        secureTextEntry={true}
+        onChangeText={text => setTextinput5(text)}
+        placeholder="Password"
+        style={styles.input}></TextInput>
+      <TextInput
+        secureTextEntry={true}
+        onChangeText={text => setTextinput6(text)}
+        placeholder="Re-enter Password"
+        style={styles.input}></TextInput>
+      <View style={{flexDirection: 'row', padding: 4, marginBottom: 5}}>
+        <CheckBox
+          disabled={false}
+          value={toggleCheckBox}
+          onCheckColor="rgb(255, 196, 0)"
+          onValueChange={newValue => setToggleCheckBox(newValue)}
+        />
+        <Text style={{marginTop: 5}}>Save Password</Text>
+      </View>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  input: {
+    borderColor: 'grey',
+    borderWidth: 1,
+    borderRadius: 12,
+    padding: 12,
+    margin: 10,
+  },
+});
+
+export default Personal;
