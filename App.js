24c21d5fa145fe8965ada0edd68ce79f5a9dfdbe
sebaei <47517850+sebaei@users.noreply.github.com>
2021-12-11 02:11:41 +0200

    Add root directory

---
+++ App.js
@@ -0,0 +1,240 @@
+import React, {useState} from 'react';
+import Personal from './src/components/Personal';
+import Family from './src/components/Family';
+import {Linking} from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  Image,
+  StyleSheet,
+  Button,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
+
+//Icons used
+import WhatsIcon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/Ionicons';
+import FamIcon from 'react-native-vector-icons/MaterialIcons';
+
+const App = () => {
+  // States that will be changed
+  const [toggleCheckBox2, setToggleCheckBox2] = useState(false);
+  const [toggleCheckBox3, setToggleCheckBox3] = useState(false);
+  const [personalPage, setPersonalPage] = useState(true);
+  const [familyPage, setFamilyPage] = useState(false);
+
+  //To enable Family Account component
+  const enfamilyPage = () => {
+    setPersonalPage(false);
+    setFamilyPage(true);
+  };
+
+  //To enable Personal Acccount component
+  const enpersonalPage = () => {
+    setFamilyPage(false);
+    setPersonalPage(true);
+  };
+
+  //Toggle checkbox beside "Sign up for notifications"
+  const togglefcheck = () => {
+    setToggleCheckBox2(!toggleCheckBox2);
+  };
+
+  //Toggle checkbox beside receive order updates
+  const togglescheck = () => {
+    setToggleCheckBox3(!toggleCheckBox3);
+  };
+
+  return (
+    <ScrollView style={styles.container}>
+      <View style={styles.wpcon}>
+        <WhatsIcon //Whatsapp Icon
+          style={styles.what}
+          name="whatsapp"
+          size={45}
+          color="green"
+          onPress={() => Linking.openURL('http://whatsapp.com')}
+        />
+        <WhatsIcon //Phone Icon
+          style={styles.phone}
+          name="phone-square"
+          size={45}
+          color="rgb(255, 196, 0)"
+          onPress={() => Linking.openURL(`tel:${+201114121838}`)}
+        />
+      </View>
+
+      {/* Rescounts Logo */}
+      <Image source={require('./assets/rescounts.png')} style={styles.logo} />
+      <Text style={styles.sup}> Sign Up</Text>
+      <Text style={styles.det}>Add your details to sign up </Text>
+      <View style={styles.btncontainer}>
+        {/* Personal Page Button */}
+        <TouchableOpacity
+          style={[styles.btn, personalPage ? styles.btnactive : styles.btn]}
+          title="Personal Account"
+          onPress={() => enpersonalPage()}>
+          <Icon
+            style={[personalPage ? styles.btnactive : styles.btninactive]}
+            name="person-sharp"
+            size={19}
+            color="grey"
+          />
+          <Text
+            style={[
+              styles.btntext,
+              personalPage ? styles.btnactive : styles.btntext,
+            ]}>
+            Personal Account
+          </Text>
+        </TouchableOpacity>
+
+        {/* Family Page Button */}
+        <TouchableOpacity
+          style={styles.btn}
+          title="Family Account"
+          onPress={() => enfamilyPage()}>
+          <FamIcon
+            style={[familyPage ? styles.btnactive : styles.btninactive]}
+            name="family-restroom"
+            size={19}
+            color="grey"
+          />
+          <Text
+            style={[
+              styles.btntext,
+              familyPage ? styles.btnactive : styles.btntext,
+            ]}>
+            Family Account
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Display Personal or Family component depending on which is pressed at the moment */}
+      {personalPage && <Personal shebop={toggleCheckBox2} />}
+      {familyPage && <Family />}
+
+      <TouchableOpacity style={styles.create} onPress={() => onSubmit()}>
+        <Text style={styles.createtext}>Create Account</Text>
+      </TouchableOpacity>
+      <View style={{flexDirection: 'row'}}>
+        <Text style={{textAlign: 'left'}}>Already have an account ?</Text>
+
+        {/* Making Login button */}
+        <TouchableOpacity style={styles.loginbtn}>
+          <Text style={{fontWeight: 'bold'}}> Login</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.signreceive}>
+        <CheckBox
+          value={toggleCheckBox2}
+          onValueChange={() => togglefcheck()}
+        />
+        <Text style={styles.checktext}>
+          Sign me up to receive exclusive offers and news on hot new restaurants
+          on Rescounts
+        </Text>
+      </View>
+      <View style={styles.signreceive}>
+        <CheckBox
+          value={toggleCheckBox3}
+          onValueChange={() => togglescheck()}
+        />
+        <Text style={styles.checktext}>Receive order updates by SMS</Text>
+      </View>
+      <Text style={({marginTop: 5}, {textAlign: 'center'})}>
+        By proceeding, you agree to our
+        <Text style={{textDecorationLine: 'underline'}}>Terms of Use</Text> and
+        confirm you have read our
+        <Text style={{textDecorationLine: 'underline'}}>
+          Privacy and Cookie Statement
+        </Text>
+      </Text>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {flex: 1, backgroundColor: 'white'},
+  wpcon: {
+    marginTop: 20,
+    flexDirection: 'row-reverse',
+  },
+  what: {
+    backgroundColor: 'green',
+    color: 'white',
+    borderRadius: 4,
+    marginRight: 15,
+  },
+  phone: {
+    backgroundColor: 'black',
+    borderRadius: 4,
+    marginRight: 30,
+    color: 'rgb(255, 196, 0)',
+  },
+  logo: {
+    backgroundColor: 'white',
+    width: 175,
+    height: 100,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+  },
+  sup: {
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+    paddingBottom: 12,
+    color: 'black',
+  },
+  det: {
+    textAlign: 'center',
+    fontSize: 16,
+    paddingBottom: 12,
+    fontWeight: 'bold',
+  },
+  btncontainer: {
+    flexDirection: 'row',
+    alignSelf: 'center',
+    backgroundColor: 'white',
+    margin: 10,
+  },
+
+  btn: {
+    flexDirection: 'row',
+    width: '50%',
+    padding: 5,
+    backgroundColor: '#FFFFFF',
+    padding: 12,
+  },
+  btntext: {fontSize: 16, textAlign: 'center', color: 'grey'},
+  btnactive: {color: 'rgb(255, 196, 0)'},
+  btninactive: {color: 'grey'},
+  create: {
+    padding: 12,
+    margin: 10,
+    marginBottom: 20,
+    borderRadius: 12,
+    backgroundColor: 'rgb(255, 196, 0)',
+  },
+  createtext: {textAlign: 'center', fontSize: 20, fontWeight: 'bold'},
+  input: {
+    borderColor: 'grey',
+    borderWidth: 1,
+    borderRadius: 12,
+    padding: 12,
+    margin: 10,
+  },
+  signreceive: {
+    flexDirection: 'row',
+    marginBottom: 9,
+  },
+  checktext: {
+    marginTop: 5,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
---
+++ src/components/Family.js
@@ -0,0 +1,204 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
+
+//Down and Right arrows icons to be used when toggling additional components on/off
+import Arr from 'react-native-vector-icons/MaterialIcons';
+
+//Eye Icon to be used in Show/Hide Password
+import Eye from 'react-native-vector-icons/FontAwesome';
+const Family = () => {
+  //First CheckBox for save password and other 3 are for an arrow icon in every component to minimize or maximize
+  const [toggleCheckBox, setToggleCheckBox] = useState(false);
+  const [farr, setFarr] = useState(true);
+  const [sarr, setSarr] = useState(false);
+  const [tarr, setTarr] = useState(false);
+
+  //Switch state for each toggle of arrows
+  const togglefarr = () => {
+    setFarr(!farr);
+  };
+  const togglesarr = () => {
+    setSarr(!sarr);
+  };
+  const toggletarr = () => {
+    setTarr(!tarr);
+  };
+  return (
+    <ScrollView style={styles.bg}>
+      <View style={[styles.card, styles.elevation]}>
+        <Text style={styles.header}>Your info</Text>
+        {farr ? (
+          <View>
+            <TextInput
+              placeholder="First Name"
+              style={styles.input}></TextInput>
+            <TextInput placeholder="Last Name" style={styles.input}></TextInput>
+            <TextInput placeholder="E-mail" style={styles.input}></TextInput>
+            <TextInput
+              placeholder="Phone Number"
+              style={styles.input}></TextInput>
+            <TextInput placeholder="Password" style={styles.input}></TextInput>
+            <TextInput
+              placeholder="Re-enter Password"
+              style={styles.input}></TextInput>
+            <View style={{flexDirection: 'row', padding: 4, marginBottom: 5}}>
+              <CheckBox
+                value={toggleCheckBox}
+                onValueChange={newValue => setToggleCheckBox(newValue)}
+              />
+              <Text style={{marginTop: 5}}>Save Password</Text>
+            </View>
+            <View>
+              <Arr
+                style={styles.arr}
+                name="keyboard-arrow-down"
+                size={50}
+                color="black"
+                onPress={() => togglefarr()}
+              />
+            </View>
+          </View>
+        ) : (
+          <View>
+            <Arr
+              style={styles.arr}
+              name="keyboard-arrow-right"
+              size={50}
+              color="black"
+              onPress={() => togglefarr()}
+            />
+          </View>
+        )}
+      </View>
+      <View style={[styles.card, styles.elevation]}>
+        <Text style={styles.header}>
+          Would you like to add your spouse to your account?
+        </Text>
+        {sarr ? (
+          <View>
+            <TextInput
+              placeholder="Spouse First Name"
+              style={styles.input}></TextInput>
+            <TextInput
+              placeholder="Spouse Last Name"
+              style={styles.input}></TextInput>
+            <TextInput
+              placeholder="Spouse Email Address"
+              style={styles.input}></TextInput>
+            <TextInput
+              placeholder="Spouse Phone Number"
+              style={styles.input}></TextInput>
+            <View>
+              <Arr
+                style={styles.arr}
+                name="keyboard-arrow-down"
+                size={50}
+                color="black"
+                onPress={() => togglesarr()}
+              />
+            </View>
+          </View>
+        ) : (
+          <Arr
+            style={styles.arr}
+            name="keyboard-arrow-right"
+            size={50}
+            color="black"
+            onPress={() => togglesarr()}
+          />
+        )}
+      </View>
+      <View style={[styles.card, styles.elevation]}>
+        <Text style={styles.header}>
+          Would you like to add your spouse to your account?
+        </Text>
+        {tarr ? (
+          <View>
+            <Text style={[{margin: 12}]}>Add First student</Text>
+            <TextInput
+              placeholder="First Name"
+              style={styles.input}></TextInput>
+            <TextInput placeholder="Last Name" style={styles.input}></TextInput>
+            <TextInput
+              placeholder="School Name"
+              style={styles.input}></TextInput>
+            <TextInput
+              placeholder="Class Name"
+              style={styles.input}></TextInput>
+            <View>
+              <TouchableOpacity>
+                <Text style={styles.addanother}>Add another student</Text>
+              </TouchableOpacity>
+              <Arr
+                style={styles.arr}
+                name="keyboard-arrow-down"
+                size={50}
+                color="black"
+                onPress={() => toggletarr()}
+              />
+            </View>
+          </View>
+        ) : (
+          <Arr
+            style={styles.arr}
+            name="keyboard-arrow-right"
+            size={50}
+            color="black"
+            onPress={() => toggletarr()}
+          />
+        )}
+      </View>
+    </ScrollView>
+  );
+};
+const styles = StyleSheet.create({
+  input: {
+    borderColor: 'grey',
+    borderWidth: 1,
+    borderRadius: 12,
+    padding: 12,
+    margin: 10,
+  },
+  card: {
+    alignSelf: 'center',
+    backgroundColor: 'white',
+    width: '95%',
+    borderRadius: 14,
+    paddingLeft: 2,
+    paddingRight: 2,
+    margin: 10,
+  },
+  elevation: {
+    elevation: 15,
+    shadowColor: 'grey',
+  },
+  header: {
+    marginTop: 10,
+    marginLeft: 12,
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: 'black',
+  },
+  bg: {
+    backgroundColor: '#f0f0ff',
+  },
+  arr: {
+    maxWidth: '100%',
+    alignSelf: 'center',
+  },
+  addanother: {
+    textAlign: 'right',
+    textDecorationLine: 'underline',
+    color: 'blue',
+  },
+});
+
+export default Family;
